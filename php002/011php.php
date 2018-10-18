f7c72bfbe9c866ccfc9718eff0a2afb675c35ce9
debello <42223093+debello@users.noreply.github.com>
2018-10-18 20:50:55 +0200

    1

--- php002/011php.php
+++ php002/011php.php
@@ -7,9 +7,35 @@ Coger una cadena donde haya varios puntos (github.io.com) y escoger la primera, 
 */
 
 //1
-echo "Hola testsdfiuasdnfuasdnf
-\n    \tsdfasdfasdf";
-print("\thola\nquease");
+// Dado una serie de números '&&\t\r1111247331111\t\n&&' queremos eliminar todos los pares, quedándonos con los números impares del medio 7 y 5
+// uso de TRIM, CHOP, Preg_Replace
+
+$siete = "\r\r&&\t\r111124779331111\t\n&&\r\r";
+$num = 1234;
+$string = "hola";
+
+
+$siete2 = trim($siete);
+echo "esto es con trim " . trim($siete) . "<br>"; // quita \r\n de ambos lados
+
+$siete3 = ltrim($siete2, "&"); //quita & de delante
+echo "esto es ltrim (sobre el anterior) " . $siete3 . "<br>";
+
+$siete4 = rtrim($siete3, "&"); //quita & de atrás
+echo "esto es rtrim (sobre el anterior) " . $siete4 . "<br>";
+
+$siete5 = preg_replace("/[1-68-9]/", "", $siete4); //quita números del 1 al 6 en el string y del 8 al 9
+echo "esto es preg_replace quitando numeros excepto 7 " . $siete5 . "<br>";
+
+$siete6 = preg_replace('/\D/', '', $siete);
+echo "esto es pre_replace quitando \t \r etc de cualquier parte " . $siete . "<br>";
+
+$siete6 = str_replace("\r\r&&", "", $siete); // reemplaza dichos elementos por espacios
+echo "esto es str_replace " . $siete6 . "<br>";
+
+echo str_replace("\r\r&&\t\r", "", str_replace("1", "", str_replace("2", "", str_replace("4", "", str_replace("9", "", str_replace("3", "", str_replace("&&\r\r", "", $siete)))))));
+
+
 
 
 
