68c622ccbed4c6b7d4576d653267fda1ab6409e4
debello <42223093+debello@users.noreply.github.com>
2018-10-18 18:01:23 +0200

    Update php002/004php.php

--- php002/004php.php
+++ php002/004php.php
@@ -1,16 +1,12 @@
-<?php
-/*
-Extraia o nome do ficheiro dunha ruta
-Exemplo de entrada : 'www.exemplo.com/public_html/index.php' --> Exemplo de saída : 'index.php' 
-
-Pista extra --> Tamén se pode facer de xeito máis áxil coa función basename
-*/
+﻿<?php
 
 // Defino la ruta en un string para el ejemplo 1
 $ruta = "Home/Usuario/Carlos/Escritorio/Ciencia/Planetas.txt";
 
 // Defino la ruta dentro del método pathinfo para el ejemplo 2
 $ruta2 = pathinfo("Home/Usuario/Carlos/Escritorio/Ciencia/Planetas.txt");
+
+
 
 // Ejemplo 1: Basename imprime automáticamente la parte final de la ruta
 echo "<strong>Usando basename:</strong> <br>" . basename($ruta) . "<br><br>";
@@ -25,5 +21,6 @@ $trozos = explode("/", $ruta);
 
 // La última posición del array es $trozos[5] y queremos conseguirla automáticamente mediante el método sizeof (o count) pero el resultado dará 6 (no cuenta desde 0) así que le restamos uno para obtener nuestro 5
 echo $trozos[sizeof($trozos)-1];
+
 
 ?>
