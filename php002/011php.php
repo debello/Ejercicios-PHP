3476caff951d099ab41f727a96c126c4923aeab1
debello <42223093+debello@users.noreply.github.com>
2018-10-18 17:59:45 +0200

    1

---
+++ php002/011php.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Chop Chop ; rtrim etc cortar cosas
+Messengizar el nombre: *__++***^^^¡¡¡'''PEpitoxx''''¡¡¡¡¡^^^^****_____+
+Cambiar texto de contenido por palabras (gatificar), sin vocales.... etc
+Coger una cadena donde haya varios puntos (github.io.com) y escoger la primera, media y ultima parte de forma separada, y las dos primeras y las dos ultimas
+*/
+
+//1
+echo "Hola testsdfiuasdnfuasdnf
+\n    \tsdfasdfasdf";
+print("\thola\nquease");
+
+
+
+?>
