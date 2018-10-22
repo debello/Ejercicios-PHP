954373ee6fbde9feefc83a7f20a37c3fd5100032
debello <42223093+debello@users.noreply.github.com>
2018-10-23 01:03:40 +0200

    1

--- php002/013html.php
+++ php002/013html.php
@@ -1,8 +1,18 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8"> 
+</head>
+<body>
+    <h3>IntroducciÃ³n</h3>
+    
+    <p>Nada</p> 
+
+    <?php
 
 //Cambiar texto de contenido por palabras (gatificar), sin vocales.... etc
 
-$texto = "El perro doméstico proviene de un ancestro o grupo ancestral común que data de hace aproximadamente 30 000 años y desde entonces se ha extendido a todas partes del mundo. Los primeros restos fósiles de perros enterrados junto con humanos fueron encontrados en Israel y datan de hace unos 12 000 años. Desde entonces, los perros y los humanos han evolucionado conjuntamente. Los perros comparten el entorno, los hábitos y el estilo de vida humanos, como las dietas ricas en cereales y almidón. La alimentación inadecuada, así como el uso de antibióticos, son la causa del desarrollo de muchas enfermedades inflamatorias e inmunológicas. Unas cuatrocientas enfermedades del perro tienen una equivalente humana, destacando especialmente la enfermedad de Alzheimer y otros trastornos neurológicos, así como cánceres, enfermedades autoinmunes y enfermedades cardiovasculares.";
+$texto = "El perro doméstico proviene de un ancestro o grupo ancestral común que data de hace aproximadamente 30 000 años y desde entonces se ha extendido a todas partes del mundo. Los primeros restos fósiles de perros enterrados junto con humanos fueron encontrados en Israel y datan de hace unos 12 000 años. Desde entonces, los perros y los humanos han evolucionado conjuntamente. Los perros comparten el entorno, los hábitos y el estilo de vida humanos, como las dietas ricas en cereales y almidón. La alimentación inadecuada, así como el uso de antibióticos, son la causa del desarrollo de muchas enfermedades inflamatorias e inmunológicas. Unas cuatrocientas enfermedades del perro tienen una equivalente humana, destacando especialmente la enfermedad de Alzheimer y otros trastornos neurológicos, así como cánceres, enfermedades autoinmunes y enfermedades cardiovasculares";
 
 echo $texto . "<br>"; // DELETE
 
@@ -42,8 +52,9 @@ $reTexto = implode(" ", $newarray);
 $reReTexto = preg_replace('/&/', "", $reTexto);
 // HASTA AQUI tenemos un array con && en lugar de las consonantes al principio de palabra // DELETE// DELETE// DELETE// DELETE// DELETE
 echo "<br> $reTexto <br>"; // DELETE
+echo "<br> $reReTexto <br>";
 
-echo mb_convert_encoding($reReTexto, "EUC-JP", "auto");
+    ?>
 
-
-?>
+</body>
+</html>
