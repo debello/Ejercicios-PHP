88287c6ae2cf43eb05ea88e76ea2f07e28e51fb1
debello <42223093+debello@users.noreply.github.com>
2018-10-30 00:01:14 +0100

    Update array001/test.php

--- array001/test.php
+++ array001/test.php
@@ -1,34 +1,15 @@
-
 <?php
+function test()
+{
+    static $count = 0;
 
- for( $b = 0; $b < 3; $b++ ) { // Loop nivel 3
-	 for( $a = 0; $a < 5; $a++ ) { // Loop nivel 2
-         echo " publish nivel 2 \$a  " . $a . "<br>";
-         for( $i = 0; $i < 10; $i++ ) { // Loop nivel 1
-              echo " publish nivel 1 \$i  " . $i . "<br>";
-                 if ($i == 2) {
-                     break 2; // Break detiene, desde $i == 2, toda ejecución del loop nivel 1
-                     echo "\$i de nivel 1: " . $i . "<br>";
-                 } 
-             echo "fuera IF \$i de nivel 1: " . $i . "<br>";
-         }
-        echo "\$a de nivel 2: " . $a . "<br>";
-		echo "end of loop <br>"; // Se sigue ejecutando esta linea que pertenece a loop nivel 2
- 	}
-     echo "\$b de nivel 3: " . $b . "<br>";
- }
-echo "test magic quotes '' \"";
-/**
-Break 1 Desde la linea detiene todo el nivel 1 del BREAK para abajo
-Break 2 Desde la linea detiene todo el nivel 2 del BREAK para abajo
-Break 3 Desde la linea detiene todo el nivel 3 del BREAK para abajo
-
-Continue detiene la ejecución actual y continúa la siguiente del loop/nivel selecionado (1)
-Continue  === Continue 1
-Continue 2 Continue detiene la ejecución actual y continúa la siguiente del loop/nivel selecionado (2)
-*/
-sdfsadf;asdfasdfsadfuidsfa dfas
-    echo hola 
-
-
+    $count++;
+    echo "uno: " . $count . "<br>";
+    if ($count < 2) {
+        test();
+    }
+    $count--;
+    echo "dos: " . $count . "<br>";
+}
+test();
 ?>
