4873fa3e7ccadc2b7a65b9284f56478ca44ac586
debello <42223093+debello@users.noreply.github.com>
2018-11-05 20:50:28 +0100

    1

--- session/salir.php
+++ session/salir.php
@@ -1,6 +1,21 @@
+<?
+
+
+?>
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title></title>
+    </head>
+
+<body>
+
 <?php
 session_start();
 session_destroy();
 echo "Ha abandonado la página. Pulse <a href='index.html'> aqui</a> para volver al inicio";
 
 ?>
+    
+</body>
+</html>
