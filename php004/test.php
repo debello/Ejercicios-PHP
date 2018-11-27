9798371f7b1684bfc55881f1bc333b290dd17eb6
debello <42223093+debello@users.noreply.github.com>
2018-11-27 16:01:32 +0100

    1

--- php004/test.php
+++ php004/test.php
@@ -1,15 +1,9 @@
-<?php
-/**
-FUNCTION - REMOVES SELECTED DUPLICATED NUMBERS IN AN ARRAY
-FUNCTION - BORRA UN NUMERO DUPLICADO ESCOGIDO DENTRO DE UN ARRAY
-*/
+<?php 
 
+$time_pre = microtime(true);
+$time_post = microtime(true);
 
+$exec_time = $time_post - $time_pre;
 
-// El array que usaremos
-$numbors = [2, 5, 7, 62, 5, 42, 7, 52, 48, 5];
-$uniqued = array_unique($numbors);
-$numbors = [2, 62, 42, 52, 48];
-print_r($uniqued);
-
+echo "$exec_time";
 ?>
