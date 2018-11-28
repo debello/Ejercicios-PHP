5f94586eddecce5bd8fde7804fc796358ab6b366
debello <42223093+debello@users.noreply.github.com>
2018-11-28 20:04:40 +0100

    1

--- 008php.php
+++ 008php.php
@@ -1,6 +1,12 @@
 <?php
 session_start();
 
+if (!isset($_POST['num'])){
+    $nem;
+    $_POST['num'] = '';   
+    
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -10,71 +16,31 @@ session_start();
     <title>Ejercicio 008 - Primos</title>  
 </head>
 <body>
-    <p>Inserte un número para saber se é ou non capicúa:</p>
+    <p>Saber num primos por debajo de: <?php if(isset($_POST['num'])) echo $_POST['num'] ?></p>
     <form action="" method="POST">
         <input type='number' name='num'/>
         <input type='submit'/>
     </form>
 <?php
 $nem = $_POST['num'];
-$primos = [2, 3, 5, 7, 11, 13, 17];
-$count = count($primos) - 1;
-
-
-    
-       for ($i = 1; $i < $nem; $i++) {
-           $c = 0;
-            for ($a = 1; $a < $nem; $a++) {
-                $z = $i / $a;
-                
-                
-                if ($i / $a === 1 || $i / $a === $i) {
-                    
-                    
-                }
-                else if ($i / $a != 1 || $i / $a != $i) {
-                    $c++;
-            }
-           echo "<br>probando c:" .$c . "<br>";
-           if($c === 17){
-               echo "$i es primo ";
+   for ($i = 2; $i <= $nem; $i++) {
+       $c = 0;
+       for ($a = 1; $a <= $i; $a++) {
+           $z = $i / $a;
+           if ($z === 1 || $z === $i) {
+                $c++;
            }
-        }
-       }
-    
-                    
-
-
-
-
-    /*
-     //Encuentra los primos (2, 3, 5, 7... del array)
-     
-       for ($i = 0; $i < $nem; $i++) {
-        for ($a = 0; $a < $count; $a++) {
            
-              
-              if ($i / $primos[$a] === 1){
-                                    echo "<br>$i es primo";
-
-              }
-              else {
-                  
-              }
-
-*/
-       
-        
-        
-        
-    
-   
-    echo "<br>el restultado de las sumas de todos los primos menores de ". $_SESSION['num']." es ". $suma;
-        
-        
-        
-        
-        
+           else if (is_float($z)) {
+                $c++;
+           }      
+        }
+       if ($c === $i){ // Si un num es primo $c equivaldrá a él (todas sus divisiones dará 1, su propio numero o numeros con decimales(float))
+           echo "$i es primo <br>";
+       }
+   }
+session_destroy();
+session_unset();
     ?>
     
 
--- session/009php.php
+++ session/009php.php
@@ -7,6 +7,8 @@ $divisor = $dni / 23;
 $noDecimals = floor($divisor);
 $dniDeci = $noDecimals * 23;
 $resto = $dni - $dniDeci;
+$hola = 
+$letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'];
 
 echo "dni número $dni <br>";
 echo "<br>divisor: " . $divisor . "<br>";
@@ -14,81 +16,16 @@ echo "nodecimals: " . $noDecimals . "<br>";
 echo "nodecimals * 23: $dniDeci";
 echo "<br>resto " . $resto ."<br>";
 
-switch ($resto) {
-        
-    case 0:
-        echo "La letra de $dni es T";
-        break;
-    case 1:
-        echo "La letra de $dni es R";
-        break;
-    case 2:
-        echo "La letra de $dni es W";
-        break;
-    case 3:
-        echo "La letra de $dni es A";
-        break;
-    case 4:
-        echo "La letra de $dni es G";
-        break;
-    case 5:
-        echo "La letra de $dni es M";
-        break;
-    case 6:
-        echo "La letra de $dni es Y";
-        break;
-    case 7:
-        echo "La letra de $dni es F";
-        break;
-    case 8:
-        echo "La letra de $dni es P";
-        break;
-    case 9:
-        echo "La letra de $dni es D";
-        break;
-    case 10:
-        echo "La letra de $dni es X";
-        break;
-    case 11:
-        echo "La letra de $dni es B";
-        break;
-    case 12:
-        echo "La letra de $dni es N";
-        break;
-    case 13:
-        echo "La letra de $dni es J";
-        break;
-    case 14:
-        echo "La letra de $dni es Z";
-        break;
-    case 15:
-        echo "La letra de $dni es S";
-        break;
-    case 16:
-        echo "La letra de $dni es Q";
-        break;
-    case 17:
-        echo "La letra de $dni es V";
-        break;
-    case 18:
-        echo "La letra de $dni es H";
-        break;
-    case 19:
-        echo "La letra de $dni es L";
-        break;
-    case 20:
-        echo "La letra de $dni es C";
-        break;
-    case 21:
-        echo "La letra de $dni es K";
-        break;
-    case 22:
-        echo "La letra de $dni es E";
-        break;
-        
-}
-    
-    
+
+
+echo "La letra de $dni es " . $letras[$resto];
+
+echo "<br><br><strong>OPCION 2</strong><br>probando a buscar el resto:" . $dni % 23 . "<br>";
+
+$resto2 = $dni % 23;
+
+echo "La letra de $dni es " . $letras[$resto2];
+
 
 
 
