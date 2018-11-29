417622bd5ddc2666fcc5c5af98285e420dadb906
debello <42223093+debello@users.noreply.github.com>
2018-11-29 20:41:30 +0100

    1

--- session/005php.php
+++ session/005php.php
@@ -1,17 +1,14 @@
 <?php
 session_start();
-if (!isset($_SESSION['numA'])){
-    $a = 0;
-    $b = 0;
-    $_SESSION['numA'] = 0;
-    $_SESSION['numB'] = 0;
-
+if (!isset($_SESSION['A'])){
+    if (!isset($_POST['Apos'])) {
+    // Declaramos las variables de sesión con un 0, número del que partirá A y B
+    $_SESSION['A'] = 0;
+    $_SESSION['B'] = 0;
+    }
 }
 else {
-    
-    echo "test: No está la session declarada?";
-    $a;
-    $b;
+        
 }
 ?>
 
@@ -19,37 +16,63 @@ else {
 <html>
 <head>
     <meta charset="UTF-8">
-    <title>Ejercicio 004 - Auntentication</title>  
+    <title>Ejercicio 005 - Aumentar, Disminuír</title> 
+    
+
+        
+</form>    
 </head>
 <body>
 
-    <p>A y B</p>
+    <p><b>Cambia el valor de A y B</b></p>
+    
+    <form method="POST" action="">
+    <input type="radio" name="Ach" value="Apos" /> Aumenta A <br>
+    <input type='radio' name="Ach" value="Aneg" /> Disminuye A <br>
+    <input type='radio' name="Ach" value="Acero"/> Resetea A <br>
+    <br>
+    <input type="radio" name="Ach" value="Bpos" /> Aumenta B <br>
+    <input type='radio' name="Ach" value="Bneg" /> Disminuye B <br>
+    <input type='radio' name="Ach" value="Bcero"/> Resetea B <br>
+    <br>
+    <input type='submit'/>
 
 <?php
+        
+    // Tenemos 6 radios, 3 de ellos dedicados a A y otros 3 a B
+    // Ambos aumentan, disminuyen y resetean
+        
+// Si hemos escogido uno de los radios
+if (isset($_POST['Ach'])){
+    // Si el radio escogido es 'Apos' (A positivo) aumentamos uno a la variable sesion A
+    if (($_POST['Ach']) === "Apos"){
+        $_SESSION['A'] = $_SESSION['A'] + 1;
+    }
+    // Lo mismo con Aneg (A negativo) disminuyendo uno
+    if (($_POST['Ach']) === "Aneg"){
+        $_SESSION['A'] = $_SESSION['A'] - 1;  
+    }
+    // Lo mismo pero igualando
+    if (($_POST['Ach']) === "Acero"){
+        $_SESSION['A'] = 0; 
+    }
     
-if (isset($_SESSION['numA'])) {
-    $_SESSION['numA'] = $_SESSION['numA'] + 1;
-echo "el valor de sesion A " . $_SESSION['numA'];
-echo "intentemos aumentarlo: <a href='005php.php'>test</a>";
-}
-    
-if (isset($_SESSION['numB'])) {
-    $_SESSION['numB'] = $_SESSION['numB'] + 1;
-echo "<br>el valor de sesion B " . $_SESSION['numB'];
-echo "intentemos aumentarlo: <a href='005php.php'>test</a>";
-}
-    
-    
-    
-    else{ echo "session num no está declarado";}
-// cuando declaro sesion num?
-    
-// if y 
-// aumentar +1 (guardarlo en session)
-// redirigir a la misma página
+    // Repetimos lo mismo para B    
+    if (($_POST['Ach']) === "Bpos"){
+        $_SESSION['B'] = $_SESSION['A'] + 1;
+    }
 
+    if (($_POST['Ach']) === "Bneg"){
+        $_SESSION['B'] = $_SESSION['A'] - 1;  
+    }
 
-    
+    if (($_POST['Ach']) === "Bcero"){
+        $_SESSION['B'] = 0;  
+    } 
+}
+    echo "<p>El valor de A es: " . $_SESSION['A'] . "</p><br>";
+    echo "<p>El valor de B es: " . $_SESSION['B'] . "</p><br>";
+        
 ?>
     
 
