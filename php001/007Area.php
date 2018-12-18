9e0558a98fb86fd3037e4efe091011ca0bf80a38
debello <42223093+debello@users.noreply.github.com>
2018-12-18 18:09:42 +0100

    1

--- php001/007Area.php
+++
@@ -1,9 +0,0 @@
-﻿<?php 
-    
-// Obtengo el número insertado en a y b del html, les asigno una variable a y b
-    $a = $_GET['a'];
-    $b = $_GET['b'];
-
-// Los multiplico en la misma frase del echo (base x altura)
-    echo "El área de un rectángulo con base $a cm y altura $b cm es ", $a*$b, " cms cuadrados";
-?>
