46b6063b8bcc72697a7eab5580c7cfebff1fa026
debello <42223093+debello@users.noreply.github.com>
2018-12-18 18:53:04 +0100

    1

--- sql/001.php
+++ sql/001.php
@@ -2,35 +2,32 @@
 
 /**
 BBDD en Mysql con PHP
-
 https://www.youtube.com/watch?v=lL8_bL24xd4&index=38&list=PLU8oAlHdN5BkinrODGXToK9oPAlnJxmW_
 
-Insertando datos de artículos en myphpadmin.
-Intento que la primera columna (SECCION etc) no sea una columna más de datos
+de 33 a 50 - Martes 18/12
 
 */
 
 
 require("datos.php");
 
-$conexion=mysqli_connect($db_host, $db_usuario, $db_contra, $db_nombre);
+$conexion = mysqli_connect($db_host, $db_usuario, $db_contra, $db_nombre);
+// equals to localhost, root, '', test2
 
-$consulta= "SELECT * FROM arti";
-
+$consulta = "SELECT * FROM arti";
 $resultados = mysqli_query($conexion, $consulta);
+//equals to mysqli_query(localhost, root, '', test2, "SELECT * FROM arti");
 
 mysqli_set_charset($conexion, "utf8");
 
-while ($fila=mysqli_fetch_row($resultados)) {
-
+while ($fila = mysqli_fetch_row($resultados)) {
 // (`SECCIÓN`, `NOMBRE ARTÍCULO`, `FECHA`, `PAÍS DE ORIGEN`, `PRECIO`)
 foreach ($fila as $k ) {
     echo $k . ", ";
-}
+	}
     echo "<br>";
-
-   
 }
 
 mysqli_close($conexion);
+
 ?>
