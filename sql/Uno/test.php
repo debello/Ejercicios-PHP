fbc62956ae4c9416e916f5c58dae670a3c04142b
debello <42223093+debello@users.noreply.github.com>
2019-01-21 20:42:41 +0100

    1

--- sql/Uno/test.php
+++ sql/Uno/test.php
@@ -1,190 +1,118 @@
 <?php
-///////////////////////////////////
-////////// OUTDATED ///////////////
-///////// ver UnoSELECT.php ///////
-///////////////////////////////////
-//////////////////////////////////
 
+/**
+
+https://www.formget.com/php-select-option-and-php-radio-button/
+http://form.guide/php-form/php-form-select.html
+
+**/
 include 'conection.php';
 
+if (isset($_POST['enviado'])) {
+    
+    $nome = $_POST['consulta1'];
+    $description = $_POST['consulta2'];
+    $prezo = $_POST['consulta3'];
+    $categoria = $_POST['consulta4'];
+ 
+    // Insertamos query
+    $query = " INSERT INTO artigo (nome, descripcion, prezo, categoria) 
+                VALUES ('$nome', '$description', $prezo, '$categoria')";
 
+    $result = $conn -> query("$query");
 
+    // Comprobamos si se ha borrado nuestra consulta seleccionada
+    if ($result === true) {   
+        echo "<br>Consulta INSERTADA con éxito";
+    }
+}
 
 ?>
+
 
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset="UTF-8">
-<style>
-    
-    #buttons {
-        height: 50px;
-    }
-    #buttons #per-button {
-        position: relative;
-        margin: -1px;
-        
-    }
-    #per-button {
-        height: 20.4px;
-        left: 33.33%;
-    }
-    #all-table {
-        position: relative;
-        float: left;
-    }
-    
-    #btn-index {
-        position: relative;
-        border-bottom; 5px;
-    }
-    #price {
-        position:relative;
-       
-  
-        display: block;
-    }
-    
-    #p1 {
-        margin: 5px;
-    }
-    #p2 {
-        margin: 5px;
-    }
-    #p3 {
-        margin: 5px;
-    }
-    
-</style>
+	<meta charset="UTF-8">
+	<link rel='stylesheet' type='text/css' href='mystylei.css'>
+	<style>
+		#btn-index3 {
+			background-color: #FFA500;
+		}
+	</style>
 </head>
 <body>
-    <h3> SELECT AND DELETE - Artigo</h3>
-    
-    <input id='btn-index' type="button" onclick="location.href='index.html';" value="INDEX"/>
-    <input id='btn-index' type="button" onclick="location.href='UnoINSERT.php';" value="INSERT"/>
-
-    
-    
-<div></div>
+    <h3> INSERT - ARTIGO</h3>
     
 
 
 
-<?php
+    
+ <?php
+    
+	/** Functión para buscar según nuestro select **/
+
+	function createTable() {
+	 $queryLEL = " SELECT  FROM ARTIGO";
+
+    $result = $conn -> query("$queryLEL");
+
+	
+	}
+
+    // Imprimimos tabla
+    $queryLEL = " SELECT * FROM ARTIGO";
+
+    $result = $conn -> query("$queryLEL");
+
+		echo "<form action='' method='GET' id='carform'>";
+
+		
+
+		echo "<select form='carform' name='hc' >";
+		while($rowLEL = mysqli_fetch_array($result)){
+			echo "<option value='" .$rowLEL['id']. "'>" .$rowLEL['nome']. "</option>";
+			$prob = $rowLEL['id'];
+			echo "probando rowlel:". $rowLEL['nome'];
+		}
+		echo "</select>";
+		echo "</form>";
+		
+	
+
+	if(isset($_POST['hc'])) {
+		echo "probando";
+	}
 
 
-            $query = " SELECT * FROM artigo";
-            $result = $conn -> query("$query");
-                        print_r($result);
-    echo "<br>";
 
-          
+
+	/*** GENERAR TODA LA TABLA 
+    echo "<table id='all-table' border=1>";
+        echo "<tr>";
+            echo "<th>id</th>";
+            echo "<th>nome</th>";
+            echo "<th>descripcion</th>";
+            echo "<th>prezo</th>";
+            echo "<th>categoria</th>";
+        echo "</tr>";
+
+	$query = " SELECT * FROM ARTIGO";
+    $result1 = $conn -> query("$query");
+
+    while($row = mysqli_fetch_array($result1)){
+        echo "<tr>";
+            echo "<td value='count_id'>" . $row['id'] . "</td>";
+            echo "<td>" . $row['nome'] . "</td>";
+            echo "<td>" . $row['descripcion'] . "</td>";
+            echo "<td>" . $row['prezo'] . "</td>";
+            echo "<td>" . $row['categoria'] . "</td>";
+        echo "</tr>";
+    }
+    echo "</table>"; ***/
     
     
-    
-    
-    /**
-
-
-
-
-    */
-    
-    
-    
-    
-
-    
-
-  
-        //$conn -> close();
-
-
-            echo "<table id='all-table' border=1>";
-                echo "<tr>";
-                    echo "<th>id_artigo</th>";
-                    echo "<th>nome</th>";
-                    echo "<th>descripcion</th>";
-                    echo "<th>prezo</th>";
-                    echo "<th>categoria</th>";
-                echo "</tr>";
-    
-            while($row = mysqli_fetch_assoc($result)){
-                //$rows[] = mysqli_fetch_array($result);
-                echo "<tr>";
-                    echo "<td value='count_id'>" . $row['id'] . "</td>";
-                    echo "<td>" . $row['nome'] . "</td>";
-                    echo "<td>" . $row['descripcion'] . "</td>";
-                    echo "<td>" . $row['prezo'] . "</td>";
-                    echo "<td>" . $row['categoria'] . "</td>";
-                echo "</tr>";
-            }
-    
-            print_r($result);
-            while($row1 = mysqli_fetch_row($result)){
-                //$rows[] = mysqli_fetch_array($result);
-                echo "<tr>";
-                    echo "<td value='count_id'>" . $row1['id'] . "</td>";
-                    echo "<td>" . $row1[0] . "</td>";
-                    echo "<td>" . $row1['descripcion'] . "</td>";
-                    echo "<td>" . $row1['prezo'] . "</td>";
-                    echo "<td>" . $row1['categoria'] . "</td>";
-                echo "</tr>";
-            }
-            echo "</table>";
-    
-    
-    
-    
-    
-    
-    /**
-
-
-
-
-    
-    
-    
-    
-
-    
-
-  
-        //$conn -> close();
-
-    $queryx = "SELECT id FROM artigo";
-    $resultx = $conn ->query($queryx);
-        print_r($resultx);
-    var_dump($resultx);
-            echo "<table id='all-table' border=1>";
-                echo "<tr>";
-                    echo "<th>id_artigo</th>";
-                echo "</tr>";
-        
-            while($row2 = mysqli_fetch_array($resultx)){
-                //$rows[] = mysqli_fetch_array($result);
-                echo "<tr>";
-                    echo "<td value='count_id'>" . $row2['id'] . "</td>";
-
-                echo "</tr>";
-            }
-            echo "</table>";
-        
-
-
-       
-        
-            Mirar SELECT, r3, 2 y porque no va la tabla
-
-
-
-
-*/
-
-
-
 ?>
-    </body>
+    
+</body>
 </html>
