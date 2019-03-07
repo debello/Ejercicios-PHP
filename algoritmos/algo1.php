c1db1812f20ff9e9287f09b6c7e7324330ae5a5c
debello <42223093+debello@users.noreply.github.com>
2019-03-07 23:59:55 +0100

    Update algo1.php

--- algoritmos/algo1.php
+++ algoritmos/algo1.php
@@ -1,60 +1,120 @@
 <?php
-/**
- * Ordenar los valores de la siguiente cadena:
- * $ex = [1 => 2, 3 => 4, 5 => 2]
- * en
- * $ex2 = [2 => (1, 5), 3 => 4];
- * 
- * To-Do: Insertar las keys respectivas de sus valores
- * line 50
- */
+    /**
+     * Ordenar los valores de la siguiente cadena:
+     * $ex = [1 => 2, 3 => 4, 5 => 2]
+     * en
+     * $ex2 = [2 => (1, 5), 3 => 4];
+     * 
+     * To-Do: Insertar las keys respectivas de sus valores
+     * line 50
+     */
 
-$ex = [1 => 2, 3 => 4, 5 => 2];
-/*
-echo $ex[0] . "<br>";
-echo $ex[0][1] . "<br>";
-echo $ex[1][0] . "<br>";
+    $ex = array('hola' => 'adios', 
+                'buenas' => 'abur', 
+                'sup' => 'adios');
 
-foreach ($ex as $k => $v) {
-    echo $k , $v . "<br>";
-}
-*/
+    function merge($arr) {
+        $exKeys = array_keys($arr);
+        $exValues = array_values($arr);
+        $new = array_combine($exValues, $exKeys);
+        return $new; 
+
+    } /* [!!!!] Se come el valor repetido (2 o 'adios') lul*/
 
 
-$exKeys = array_keys($ex);
-$exValues = array_values($ex);
+    /* Intentar cambiar valores x keys y viceversa*/
 
-echo $exKeys[0] . "<br>";
-echo $exKeys[1] . "<br>";
-echo $exKeys[2] . "<br>";
-echo "<br>";
-echo $exValues[0] . "<br>";
-echo $exValues[1] . "<br>";
-echo $exValues[2] . "<br>";
-$exSize = sizeof($ex);
-echo "<br> esto es el tamaño del array $exSize <br>";
-/* 
-Escogemos 3 como el size porque de lo contrario cuando i sea 0
-a será 0 y cuando i sea 1 a será 0... entonces no 'arrancará'
-y para cuando lo haga i será 2 y no lo podremos comparar $a con el
-siguiente valor 
-todo: aclarar la explicación
-*/
-for ($i = 0; $i < 3; $i++){ 
-    for ($a = 0; $a < $i; $a++){
-        if ($exValues[$a] === $exValues[$i]) {
-            echo "Hemos insertado un valor<br>";
-            $exArray[] = $exValues[$a];
-            /*
-            $exArray[$a] = $exKeys[$a];
-            $exArray[$a] = $exKeys[$i];
-            */
+    $revEx = merge($ex);
+    var_dump($revEx);
+    foreach ($revEx as $k => $v) {
+        echo 'test' .$k , $v . "<br>";
+    }
+
+
+
+
+
+
+
+    /* CANCELLED?
+    * muy complicado
+    *  focus en cambiar los valores y keys directamente en el array
+    * 
+    * 
+    * /
+
+    echo $ex[0] . "<br>";
+    echo $ex[0][1] . "<br>";
+    echo $ex[1][0] . "<br>";
+
+    foreach ($ex as $k => $v) {
+        echo $k , $v . "<br>";
+    }
+
+
+
+    $exKeys = array_keys($ex);
+    $exValues = array_values($ex);
+
+    echo $exKeys[0] . "<br>";
+    echo $exKeys[1] . "<br>";
+    echo $exKeys[2] . "<br>";
+    echo "<br>";
+    echo $exValues[0] . "<br>";
+    echo $exValues[1] . "<br>";
+    echo $exValues[2] . "<br>";
+    $exSize = sizeof($ex);
+    echo "<br> test: <br>" .  $ex[$exKeys[2]];
+    $ex[$exKeys[2]][] = 5;
+    /* 
+    Escogemos 3 como el size porque de lo contrario cuando i sea 0
+    a será 0 y cuando i sea 1 a será 0... entonces no 'arrancará'
+    y para cuando lo haga i será 2 y no lo podremos comparar $a con el
+    siguiente valor 
+    todo: aclarar la explicación
+
+    for ($i = 0; $i < 3; $i++){ 
+        for ($a = 0; $a < $i; $a++){
+            if ($exValues[$i] === $exValues[$a]) {
+                echo "Hemos insertado un valor $exValues[$i]<br>";
+                // Values (2 from [0][1] and 2 from [2][1])
+                $exArray[] = $exValues[$i];
+                // Keys (1 from [0][0] and 5 from [2][0])
+                $exArray2[] = $exKeys[$a];
+                $exArray2[] = $exKeys[$i];
+                /*
+                $exArray[$a] = $exKeys[$a];
+                $exArray[$a] = $exKeys[$i];
+                
+            }
+            else {
+                //$exClean[] = $ex
+            }
         }
     }
-}
-foreach ($exArray as $k => $v) {
-    echo $k , $v . "<br>";
-}
+    foreach ($exArray as $k => $v) {
+        echo $k , $v . "<br>";
+    }
+    echo "<br>";
+    foreach ($exArray2 as $k => $v) {
+        echo $k , $v . "<br>";
+    }
+    echo "<br>";
+    $newArr[] = $exArray;
+    $newArr[0] = $exArray2;
 
+    foreach ($newArr[0] as $k => $v) {
+        echo $k , $v . "<br>";
+    }
+
+    var_dump($newArr);
+    */
+    /* 
+    array(2) { [0]=> array(1) 
+        { [0]=> int(2) } [2]=> array(2) 
+        { [0]=> int(1) [1]=> int(5) }}
+
+
+    */
 
 ?>
